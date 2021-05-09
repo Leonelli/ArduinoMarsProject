dda98c0b9b5cfc33d0947b46d50b58ace7a45ed8
Leonelli <male00004@stud.uni-saarland.de>
2021-05-09 14:18:38 +0200

    working demo

--- F7P3LDOIYOQISM2.ino
+++ F7P3LDOIYOQISM2.ino
@@ -57,147 +57,6 @@ NextDotDash:
 
 
 
-
-String ascii_to_hex_convert(char c){
-  if(c =='A'){
-        Serial.println("41");
-        rotateServo("41");
-  }
-
- else if(c=='B'){
-        Serial.println("42");
-        rotateServo("42");
-  }
-
-else if(c=='C'){
-        Serial.println("43");
-        rotateServo("43");
-  }
-
-else if(c=='D'){
-        Serial.println("44");
-        rotateServo("44");
-  }
-
-else if(c=='E'){
-        Serial.println("45");
-        rotateServo("45");
-}
-else if(c=='F'){
-        Serial.println("46");
-        rotateServo("46");
-  }
-
-else if(c=='G'){
-        Serial.println("47");
-        rotateServo("47");
-  }
-
-else if(c=='H'){
-        Serial.println("48");
-        rotateServo("48");
-  }
-
-else if(c=='I'){
-        Serial.println("49");
-        rotateServo("49");
-  }
-
-else if(c=='J'){
-        Serial.println("4A");
-        rotateServo("4A");
-  }
-
-else if(c=='K'){
-        Serial.println("4B");
-        rotateServo("4B");
-}
-
-else if(c=='L'){
-        Serial.println("4C");
-        rotateServo("4C");
-  }
-
-else if(c=='M'){
-        Serial.println("4D");
-        rotateServo("4D");
-  }
-
-else if(c=='N'){
-        Serial.println("4E");
-        rotateServo("4E");
-  }
-
-else if(c=='O'){
-        Serial.println("4F");
-        rotateServo("4F");
-  }
-
-else if(c=='P'){
-        Serial.println("50");
-        rotateServo("50");
-  }
-
-else if(c=='Q'){
-        Serial.println("51");
-        rotateServo("51");
-  }
-else if(c=='R'){
-        Serial.println("52");
-        rotateServo("52");
-  }
-
- else if(c=='S'){
-        Serial.println("53");
-        rotateServo("53");
-  }
-
- else if(c=='T'){
-        Serial.println("54");
-        rotateServo("54");
-  }
-
- else if(c=='U'){
-        Serial.println("55");
-        rotateServo("55");
-  }
-
- else if(c=='V'){
-        Serial.println("56");
-        rotateServo("56");
-  }
-
- else if(c=='W'){
-        Serial.println("57");
-        rotateServo("57");
-  }
-
- else if(c=='X'){
-        Serial.println("58");
-        rotateServo("58");
-  }
-
- else if(c=='Y'){
-        Serial.println("59");
-        rotateServo("59");
-  }
-
- else if(c=='Z'){
-        Serial.println("5A");
-        rotateServo("5A");
-  }
-  
-}   
-
-
-void ResetPosition(int angle){
-  for(int servoAngle = angle; servoAngle >= 0; servoAngle--)  //move the micro servo from 0 degrees to 180 degrees
-  {                                  
-    servo.write(servoAngle);              
-    delay(10);                  
-  }
-}
-
 void rotateServo(String hex_value_returned){
     Serial.println("hex_value_returned:"+hex_value_returned);
 
@@ -287,9 +146,164 @@ void rotateServo(String hex_value_returned){
        }
      
     }
-
 }
 
+
+
+void ascii_to_hex_convert(char c){     
+  Serial.println(c);
+  if(c =='A'){ 
+ 
+        Serial.println("41");
+        rotateServo("41");
+                Serial.println("function running");
+
+  }
+
+  if(c=='B'){
+        Serial.println("42");
+        rotateServo("42");
+                Serial.println("function running");
+
+  }
+
+ if(c=='C'){
+        Serial.println("43");
+        rotateServo("43");
+                Serial.println("function running");
+
+  }
+
+ if(c=='D'){
+        Serial.println("44");
+        rotateServo("44");
+  }
+
+ if(c=='E'){
+        Serial.println("45");
+        rotateServo("45");
+                Serial.println("function running");
+
+}
+ if(c=='F'){
+        Serial.println("46");
+        rotateServo("46");
+  }
+
+ if(c=='G'){
+        Serial.println("47");
+        rotateServo("47");
+  }
+
+ if(c=='H'){
+        Serial.println("48");
+        rotateServo("48");
+  }
+
+ if(c=='I'){
+        Serial.println("49");
+        rotateServo("49");
+  }
+
+ if(c=='J'){
+        Serial.println("4A");
+        rotateServo("4A");
+  }
+
+ if(c=='K'){
+        Serial.println("4B");
+        rotateServo("4B");
+}
+
+ if(c=='L'){
+        Serial.println("4C");
+        rotateServo("4C");
+  }
+
+ if(c=='M'){
+        Serial.println("4D");
+        rotateServo("4D");
+  }
+
+ if(c=='N'){
+        Serial.println("4E");
+        rotateServo("4E");
+  }
+
+ if(c=='O'){
+        Serial.println("4F");
+        rotateServo("4F");
+  }
+
+ if(c=='P'){
+        Serial.println("50");
+        rotateServo("50");
+  }
+
+ if(c=='Q'){
+        Serial.println("51");
+        rotateServo("51");
+        Serial.println("function running");
+
+  }
+ if(c=='R'){
+        Serial.println("52");
+        rotateServo("52");
+                Serial.println("function running");
+
+  }
+
+  if(c=='S'){
+        Serial.println("53");
+        rotateServo("53");
+  }
+
+  if(c=='T'){
+        Serial.println("54");
+        rotateServo("54");
+  }
+
+  if(c=='U'){
+        Serial.println("55");
+        rotateServo("55");
+  }
+
+  if(c=='V'){
+        Serial.println("56");
+        rotateServo("56");
+  }
+
+  if(c=='W'){
+        Serial.println("57");
+        rotateServo("57");
+  }
+
+  if(c=='X'){
+        Serial.println("58");
+        rotateServo("58");
+  }
+
+  if(c=='Y'){
+        Serial.println("59");
+        rotateServo("59");
+  }
+
+  if(c=='Z'){
+        Serial.println("5A");
+        rotateServo("5A");
+  }
+  
+  
+}   
+
+
+void ResetPosition(int angle){
+  for(int servoAngle = angle; servoAngle >= 0; servoAngle--)  //move the micro servo from 0 degrees to 180 degrees
+  {                                  
+    servo.write(servoAngle);              
+    delay(10);                  
+  }
+}
 
 char readio()
 {
@@ -319,10 +333,8 @@ void convertor()
     {
       if (letters[i] == code)
       {
-        char c = char('A' + i);
-        Serial.println(c);
-        code = "";                  
-        ascii_to_hex_convert(c);
+        Serial.println(char('A' + i));
+        ascii_to_hex_convert(char('A' + i));
         break;
        
       }
